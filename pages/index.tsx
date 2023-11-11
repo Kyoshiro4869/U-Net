c46cdb03a8fd62734067e14e145c3e7a069a5076
Kyoshiro4869 <147650441+Kyoshiro4869@users.noreply.github.com>
2023-11-11 23:49:18 +0900

    Commit from Stub AI (#1)
    Co-authored-by: Kyoshiro4869 <bot@codestub.ai>

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,16 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+      <h1 className="text-4xl font-bold">Job Postings</h1>
+      <div className="flex flex-wrap justify-center">
+        {/* Replace this with actual job data */}
+        {Array(10).fill(null).map((_, index) => (
+          <div key={index} className="m-4 p-4 border rounded shadow">
+            <h2 className="text-xl font-bold">Job Title {index + 1}</h2>
+            <p>Job Description {index + 1}</p>
+          </div>
+        ))}
+      </div>
     </main>
   )
 }

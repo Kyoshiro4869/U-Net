967e4514d922671dee3b3eac92ed0ba3c6844f04
Kyoshiro4869 <147650441+Kyoshiro4869@users.noreply.github.com>
2023-11-12 00:20:28 +0900

    Commit from Stub AI (#6)
    Co-authored-by: Kyoshiro4869 <bot@codestub.ai>

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,16 +8,16 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex flex-col items-center justify-between min-h-screen p-24 ${inter.className}`}
     >
       <h1 className="font-bold text-4xl">Job Postings</h1>
       <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {/* Replace this with actual job data */}
         {Array(10).fill(null).map((_, index) => (
           <Link href={`/job/${index + 1}`} key={index}>
-            <a className="flex flex-col items-center p-4 border rounded shadow">
+            <a className="flex flex-col items-center border rounded shadow p-4">
               <Image src="/path/to/image.jpg" alt="Job Image" width={100} height={75} />
-              <h2 className="mt-4 font-bold text-xl">JOB TITLE {index + 1}</h2>
+              <h2 className="font-bold text-xl mt-4">JOB TITLE {index + 1}</h2>
               <p className="mt-2">Job Type: Long-term Internship</p>
               <div className="flex items-center mt-2">
                 <Image src="/path/to/logo.jpg" alt="Company Logo" width={25} height={25} />

1837e768891fbcb24a4f8ba49df745cf9f0a6493
Kyoshiro4869 <147650441+Kyoshiro4869@users.noreply.github.com>
2023-11-12 00:02:16 +0900

    Commit from Stub AI (#4)
    Co-authored-by: Kyoshiro4869 <bot@codestub.ai>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { FaBookmark } from 'react-icons/fa'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,18 +14,20 @@ export default function Home() {
       <div className="flex flex-wrap justify-center">
         {/* Replace this with actual job data */}
         {Array(10).fill(null).map((_, index) => (
-          <div key={index} className="m-4 p-4 border rounded shadow flex flex-col items-center">
-            <Image src="/path/to/image.jpg" alt="Job Image" width={500} height={300} />
-            <h2 className="text-xl font-bold mt-4">JOB TITLE {index + 1}</h2>
-            <p className="mt-2">Job Type: Long-term Internship</p>
-            <div className="flex items-center mt-2">
-              <Image src="/path/to/logo.jpg" alt="Company Logo" width={50} height={50} />
-              <p className="ml-2">Company Name</p>
-            </div>
-            <div className="mt-2">
-              <FaBookmark size={24} />
-            </div>
-          </div>
+          <Link href={`/job/${index + 1}`} key={index}>
+            <a className="m-4 p-4 border rounded shadow flex flex-col items-center">
+              <Image src="/path/to/image.jpg" alt="Job Image" width={200} height={150} />
+              <h2 className="text-xl font-bold mt-4">JOB TITLE {index + 1}</h2>
+              <p className="mt-2">Job Type: Long-term Internship</p>
+              <div className="flex items-center mt-2">
+                <Image src="/path/to/logo.jpg" alt="Company Logo" width={50} height={50} />
+                <p className="ml-2">Company Name</p>
+              </div>
+              <div className="mt-2">
+                <FaBookmark size={24} />
+              </div>
+            </a>
+          </Link>
         ))}
       </div>
     </main>
